aac530ba257d58551719b4ee6f3c1ccc213d18da
Noah <nbiggs1337@gmail.com>
2021-04-23 14:16:08 -0500

    Loop Challenges Push

---
+++ Assignments/JS/Loop_Challenges/2-PrintSequence.js
@@ -0,0 +1,3 @@
+for (var i=4; i>-4; i-=1.5){
+    console.log(i)
+}
---
+++ Assignments/JS/Loop_Challenges/4-factorial.js
@@ -0,0 +1,8 @@
+var product=0
+var n = 5
+
+for (var i=1;product<1e8;i++){
+    product = n*i
+}
+console.log(i + " is i")
+console.log(product + " is the product.")
