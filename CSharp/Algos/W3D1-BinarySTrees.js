26daab1901db25bf9ed01e5a6b36ebe4c1d9e0df
Noah <nbiggs1337@gmail.com>
2021-06-21 09:56:40 -0500

    Morning Algos- BST Constructor + Base methods

---
+++ CSharp/Algos/W3D1-BinarySTrees.js
@@ -0,0 +1,71 @@
+//Morning Algos Jun 21 2021
+//Data Structures
+//Binary Search Trees
+
+
+class node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+    
+    //methods
+    
+    printMin(){
+        var runner = this.root;
+        while (runner.left != null){
+            runner = runner.left;
+        }
+        console.log(runner.value);
+        return runner.value;
+    }
+    
+    printMax(){
+        var runner = this.root;
+        while (runner.right != null){
+            runner = runner.right;
+        }
+        console.log(runner.value);
+        return runner.value;
+    }
+    
+    add(value){
+        var newNode = new Node(value);
+        if (this.root == null){
+            this.root = newNode;
+            return this;
+        }
+        else {
+            var runner = this.root;
+            while( true ) {
+                if (runner.value < value){
+                    if (runner.right == null){
+                        runner.right = newNode;
+                        return this;
+                    }
+                    runner = runner.right;
+                }
+                else {
+                    if(runner.left == null){
+                        runner.left = newNode;
+                        return this;
+                    }
+                    runner = runner.left;
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    
+}
