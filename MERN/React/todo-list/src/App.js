d639c9916ae7d6e6ad00c3b7b0de2d065cdd34d7
Noah <nbiggs1337@gmail.com>
2021-07-12 12:24:52 -0500

    ToDo list assignment

---
+++ MERN/React/todo-list/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
