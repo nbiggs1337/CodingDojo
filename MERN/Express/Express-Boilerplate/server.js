397cca80ebf5f18537048d16fcf5c6bd3d659e39
Noah <nbiggs1337@gmail.com>
2021-07-14 13:44:50 -0500

    Express Server test

--- MERN/Express/Express-Boilerplate/server.js
+++ MERN/Express/Express-Boilerplate/server.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const app = express();
+// make sure these lines are above any app.get or app.post code blocks
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+
+const users = [
+  { firstName: "Reimu",  lastName: "Hakurei"    },
+  { firstName: "Marisa", lastName: "Kirisame"   },
+  { firstName: "Sanae",  lastName: "Kochiya"    },
+  { firstName: "Sakuya", lastName: "Izayoi"     },
+  { firstName: "Momiji", lastName: "Inubashiri" }
+];
+  
+app.get("/api/users", (req, res) => {
+  console.log("in the users")
+  res.json({ users :users});
+});
 
 // req is short for request
 // res is short for response
 app.get("/api", (req, res) => {
   res.send("Our express api server is now sending this over to the browser");
 });
+
+app.get("/api/users/:id", (req, res) => {
+  // we can get this `id` variable from req.params
+  console.log(req.params.id);
+  // assuming this id is the index of the users array we could return one user this way
+  res.json( users[req.params.id] );
+});
+
+app.put("/api/users/:id", (req, res) => {
+  // we can get this `id` variable from req.params
+  const id = req.params.id;
+  // assuming this id is the index of the users array we can replace the user like so
+  users[id] = req.body;
+  // we always need to respond with something
+  res.json( { status: "ok" } );
+});
+
+app.post("/api/users", (req, res) => {
+  // req.body will contain the form data from Postman or from React
+  console.log(req.body);
+  // we can push it into the users array for now...
+  // later on this will be inserted into a database
+  users.push(req.body);
+  // we always need to respond with something
+  res.json( { status: "ok" } );
+});
+
 
 const server = app.listen(8000, () =>
   console.log(`Server is locked and loaded on port ${server.address().port}!`)
